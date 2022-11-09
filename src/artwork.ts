f04af28ecf6877692c0a38017c6ea0387189ffb4
Fuzi18 <fuzesi.t2003@gmail.com>
2022-11-09 13:01:04 +0100

    xd

---
+++ src/artwork.ts
@@ -0,0 +1,14 @@
+
+class Statue implements Artwork{
+    title: string;
+    year: Date;
+    price:number;
+    #height:number;
+    constructor(title:string,year:Date, price:number, height:number){
+        this.title=title;
+        this.year=year;
+        this.price=price;
+        this.#height=height;
+    }
+   
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
+let title=/[a-zA-Z ,  ]{1}/;
+let year=/[]/;
+
+
 interface Artwork{
     title:string;
     year:Date;
     price:number;
 }
 
-class Statue implements Artwork{
-    title: string;
-    year: Date;
-    price:number;
-    #height:number;
-    constructor(title:string,year:Date, price:number, height:number){
-        this.title=title;
-        this.year=year;
-        this.price=price;
-        this.#height=height;
-    }
-   
-}
+
 
 
 document.getElementById("gomb")?.addEventListener("click", ()=>{
